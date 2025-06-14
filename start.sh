888109fa5cc0cc93f53de802e780184d07d29501
Ubuntu <myweblearn@myweblearn.wgouhrnmz3qepgtf3pgayad4lh.lx.internal.cloudapp.net>
2025-06-14 12:19:30 +0000

    deteleted

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🚀 Starting Video Downloader & YouTube Uploader..."
-pm2 start ecosystem.config.js
-pm2 save
-echo "✅ Application started successfully!"
-echo "🌐 Access your application at: https://prafunschool.web.id"
