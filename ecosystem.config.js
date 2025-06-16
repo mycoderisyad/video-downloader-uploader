92395b518b630da5128d905cd312e9d4f17b7f3e
Ubuntu <myweblearn@myweblearn.wgouhrnmz3qepgtf3pgayad4lh.lx.internal.cloudapp.net>
2025-06-16 20:27:19 +0000

    Fix PM2 configuration: Use fork mode instead of cluster to prevent port conflicts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'video-downloader',
     script: 'server.js',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
